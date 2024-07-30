9ea819863b447b6f912a5771c157bee4e93dc0cd
tadahiroueta <tadahiroueta@gmail.com>
2024-07-29 23:32:11 -0500

    2024 update as class with proper comments

--- sdk.py
+++ sdk.py
@@ -1,144 +1,417 @@
-def column(x):
-    column = []
-    for i in board:
-        column.append(i[x])
+class Game:
+    """
+        A class to represent the game.
+        On initialization, the user is prompted to input the board, and a solution will be printed.
 
-    return column
+    Attributes
+    ----------
+    INTRODUCTION : str
+        the introduction to the game
+    board : list
+        the board
+    possibilities : list
+        the possibilities for each cell
+    checkpoints : list
+        the checkpoints
 
+    Methods
+    -------
+    getBoard()
+        Get the board from the user.
+    printSolution()
+        Print the solved board.
+    getRow(y, matrix=None)
+        Get a row from the board.
+    getColumn(x, matrix=None)
+        Get a column from the board.
+    getBox(x, y, matrix=None)
+        Get a box from the board.
+    getCellPossibilities(x, y)
+        Get the possibilities for a cell.
+    getPotentialLocations(n, groupPossibilities)
+        Get the potential locations for a number in a group.
+    isSolved()
+        Check if the board is solved.
+    solveCells()
+        Solve cells one at a time, once, by process of elimination.
+    findMissingNumber(n, group, groupPossibilities)
+        Find a missing number in a group.
+    solveMissingNumbers()
+        Solve missing numbers in groups.
+    guess()
+        Guess the first possibility of the first unresolved cell to be right, and save the current state in the likely case it was a bad guess.
+    solve()
+        Solve the board with multiple different algorithms (including blind trial and error).
+    """
 
-def box(x, y):
-    global board
-    square = []
-    for yy in range(3):
-        for xx in range(3):
-            square.append(board[yy + y // 3 * 3][xx + x // 3 * 3])
+    INTRODUCTION = "Type in your sodoku. Like so:\n" + \
+        "    1 0 0 0 0 0 0 0 0\n" + \
+        "    0 2 0 0 0 0 0 0 0\n" + \
+        "    0 0 3 0 0 0 0 0 0\n" + \
+        "    0 0 0 0 0 4 0 0 0\n" + \
+        "    0 0 0 0 5 0 0 0 0\n" + \
+        "    0 0 0 6 0 0 0 0 0\n" + \
+        "    0 0 0 0 0 0 7 0 0\n" + \
+        "    0 0 0 0 0 0 0 8 0\n" + \
+        "    0 0 0 0 0 0 0 0 9\n"
+    
+    def __init__(self) -> None:
+        self.possibilities = [[[] for _ in range(9)] for _ in range(9)]
+        self.checkpoints = []
 
-    return square
+        print(self.INTRODUCTION)
+        # testing empty game
+        self.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        self.solve()
+        self.printSolution()
 
+    # I/O
 
-print("Type in your sodoku. Like so:\n" +
-      "    1 0 0 0 0 0 0 0 0\n" +
-      "    0 2 0 0 0 0 0 0 0\n" +
-      "    0 0 3 0 0 0 0 0 0\n" +
-      "    0 0 0 0 0 4 0 0 0\n" +
-      "    0 0 0 0 5 0 0 0 0\n" +
-      "    0 0 0 6 0 0 0 0 0\n" +
-      "    0 0 0 0 0 0 7 0 0\n" +
-      "    0 0 0 0 0 0 0 8 0\n" +
-      "    0 0 0 0 0 0 0 0 9\n")
+    def getBoard(self) -> list:
+        """
+        Get the board from the user (0 represents a blank cell).
 
-board = []
-for i in range(9):
-    board.append(input(">>>    ").split(' '))
+        Returns
+        -------
+        list
+            a list of lists representing the board
+        """
+        board = []
+        for _ in range(9):
+            board.append(input(">>>    ").split(' '))
 
-# Seid ihr das Essen? Nein, wir sind der Jäger!
-# to stop and edit
-blank = 0
-past = []
-for y in range(9):
-    for x in range(9):
-        board[y][x] = int(board[y][x])
-        if not board[y][x]:
-            board[y][x] = []
-            blank += 1
+        for y in range(9):
+            for x in range(9):
+                board[y][x] = int(board[y][x])
 
-shadow = 82
-while blank:
-    # empty method
-    for y in range(9):
+        try:
+            assert len(board) == 9
+            for row in board:
+                assert len(row) == 9
+
+        except AssertionError:
+            print("Invalid board.")
+            return self.getBoard()
+
+        return board
+    
+    def printSolution(self) -> None:
+        """
+        Print the solved board.
+        """
+        print("\nHere you go:")
+        for y in range(9):
+            for x in range(9):
+                if not x: print()
+                print(self.board[y][x], end=' ')
+
+    # helper
+    
+    def getRow(self, y, matrix=None) -> list:
+        """
+        Get a row from the board.
+
+        Parameters
+        ----------
+        y : int
+            the row to get
+        matrix : list, optional
+            the matrix to get the row from (default is self.board)
+
+        Returns
+        -------
+        list
+            a list representing the row
+        """
+        if not matrix: matrix = self.board
+
+        return matrix[y]
+    
+    def getColumn(self, x, matrix=None) -> list:
+        """
+        Get a column from the board.
+
+        Parameters
+        ----------
+        x : int
+            the column to get
+        matrix : list, optional
+            the matrix to get the column from (default is self.board)
+
+        Returns
+        -------
+        list
+            a list representing the column
+        """
+        if not matrix: matrix = self.board
+        column = []
+        for row in matrix:
+            column.append(row[x])
+
+        return column
+    
+    def getBox(self, x, y, matrix=None) -> list:
+        """
+        Get a box from the board.
+
+        Parameters
+        ----------
+        x : int
+            the x-coordinate of the box
+        y : int
+            the y-coordinate of the box
+        matrix : list, optional
+            the matrix to get the box from (default is self.board)
+
+        Returns
+        -------
+        list
+            a list representing the box
+        """
+        if not matrix: matrix = self.board
+
+        square = []
+        for yy in range(3):
+            for xx in range(3):
+                square.append(matrix[yy + y // 3 * 3][xx + x // 3 * 3])
+
+        return square
+    
+    def getCellPossibilities(self, x, y) -> list:
+        """
+        Get the possibilities for a cell.
+
+        Parameters
+        ----------
+        x : int
+            the x-coordinate of the cell
+        y : int
+            the y-coordinate of the cell
+
+        Returns
+        -------
+        list
+            a list of possibilities for the cell
+        """
+        possibilities = []
+        for n in range(1, 10):
+            if n not in self.getRow(y) and n not in self.getColumn(x) and n not in self.getBox(x, y):
+                possibilities.append(n)
+
+        return possibilities
+
+    def getPotentialLocations(self, n, groupPossibilities) -> list:
+        """
+        Get the potential locations for a number in a group.
+        
+        Parameters
+        ----------
+        n : int
+            the number to find
+        groupPossibilities : list
+            the possibilities for the group
+
+        Returns
+        -------
+        list
+            a list of potential locations for the number
+        """
+        potentialLocations = []
         for x in range(9):
-            lone = []
-            if isinstance(board[y][x], list):
-                # possibilities
-                board[y][x] = []
-                for n in range(1, 10):
-                    # conditions
-                    if n not in board[y] and n not in column(x) and n not in box(x, y):
-                        board[y][x].append(n)
+            if n in groupPossibilities[x]: potentialLocations.append(x)
+        
+        return potentialLocations
 
-                # only one option
-                if len(board[y][x]) == 1:
-                    board[y][x] = board[y][x][0]
-                    blank -= 1
+    def isSolved(self) -> bool:
+        """
+        Check if the board is solved.
 
-                # bad guess
-                elif len(board[y][x]) == 0:
-                    while True:
-                        if past[-1][3] + 1 < past[-1][4]:
-                            # matrix shit
-                            chuugoku = []
-                            for i in past[-1][0]:
-                                chuugoku.append(i[:])
+        Returns
+        -------
+        bool
+            whether the board is solved
+        """
+        for y in range(9):
+            for x in range(9):
+                if self.board[y][x] == 0: return False
 
-                            board = chuugoku
-                            blank = past[-1][5]
-                            past[-1][3] += 1
+        return True
 
-                            # stroke
-                            (board[past[-1][1]]
-                                  [past[-1][2]]) = (board[past[-1][1]]
-                                                         [past[-1][2]]
-                                                         [past[-1][3]])
-                            break
+    # solving algorithms
+    
+    def solveCells(self) -> bool:
+        """
+        Solve cells one at a time, once, by process of elimination (i.e. only one number can go in a cell).
+        
+        Returns
+        -------
+        bool
+            whether any cells were solved
+        """
 
-                        else:
-                            past.pop(-1)
+        solvedAny = False
 
-    if blank == shadow:
-        # number method
-        for i in range(9):
-            for n in range(1, 10):
-                form = i % 3 * 3
-                ula = i // 3 * 3
-                for c in range(3):
-                    circuits = [board[i], column(i), box(form, ula)]
-                    if n not in circuits[c]:
-                        lone = []
-                        for j in range(9):
-                            if isinstance(circuits[c][j], list):
-                                if n in circuits[c][j]:
-                                    lone.append(j)
+        # for each cell
+        for y in range(9): 
+            for x in range(9):
 
-                        # only one option
-                        if len(lone) == 1:
-                            if circuits[c] == board[i]:
-                                board[i][lone[0]] = n
+                # already solved
+                if self.board[y][x] != 0: continue
 
-                            elif circuits[c] == column(i):
-                                board[lone[0]][i] = n
+                possibilities = self.getCellPossibilities(x, y)
 
-                            else:
-                                (board[ula + lone[0] // 3]
-                                      [form + lone[0] % 3]) = n
+                # guessed wrong
+                if not possibilities: raise Exception("Wrong guess")
 
-                            blank -= 1
+                # too many possibilities
+                if len(possibilities) > 1:
+                    self.possibilities[y][x] = possibilities
+                    continue
 
-        if blank == shadow:
-            # find first uncertainty
+                # solved                
+                self.board[y][x] = possibilities[0]
+                self.possibilities[y][x] = []
+                solvedAny = True
+        
+        return solvedAny
+
+    def findMissingNumber(self, n, group, groupPossibilities) -> int | None:
+        """
+        Find a missing number in a group.
+
+        Parameters
+        ----------
+        n : int
+            the number to find
+        group : list
+            the group to search
+        groupPossibilities : list
+            the possibilities for the group
+
+        Returns
+        -------
+        int
+            the location of the missing number
+        """
+
+        # not missing
+        if n in group: return
+
+        potentialLocations = self.getPotentialLocations(n, groupPossibilities)
+
+        # guessed wrong
+        if not potentialLocations: raise Exception("Wrong guess")
+        
+        # too many possibilities
+        if len(potentialLocations) > 1: return
+
+        # found solution
+        return potentialLocations[0]
+
+    def solveMissingNumbers(self) -> bool:
+        """
+        Solve missing numbers in groups (e.g. rows, columns, boxes).
+        
+        Returns
+        -------
+        bool
+            whether any numbers were solved
+        """
+
+        solvedAny = False
+
+        # for each potentially missing number
+        for n in range(1, 10):
+
+            # rows
             for y in range(9):
-                for x in range(9):
-                    if isinstance(board[y][x], list):
-                        blank -= 1
+                solution = self.findMissingNumber(n, self.getRow(y), self.getRow(y, self.possibilities))
+                if solution: 
+                    self.board[y][solution] = n
+                    self.possibilities[y][solution] = []
+                    solvedAny = True
 
-                        # copies matrix
-                        chuugoku =[]
-                        for i in board:
-                            chuugoku.append(i[:])
+            # columns
+            for x in range(9):
+                solution = self.findMissingNumber(n, self.getColumn(x), self.getColumn(x, self.possibilities))
+                if solution:
+                    self.board[solution][x] = n
+                    self.possibilities[solution][x] = []
+                    solvedAny = True
+            
+            # boxes
+            for y in range(0, 9, 3):
+                for x in range(0, 9, 3):
+                    solution = self.findMissingNumber(n, self.getBox(x, y), self.getBox(x, y, self.possibilities))
+                    if solution: 
+                        self.board[y + solution // 3][x + solution % 3] = n
+                        self.possibilities[y + solution // 3][x + solution % 3] = []
+                        solvedAny = True
 
-                        past.append([chuugoku, y, x, 0, len(chuugoku[y][x]), blank])
-                        board[y][x] = board[y][x][0]
-                        stop = True
-                        break
+        return solvedAny
+    
+    def guess(self) -> None:
+        """
+        Guess the first possibility of the first unresolved cell to be right, and save the current state in the likely case it was a bad guess.
+        """
 
-                if stop:
-                    stop = False
-                    break
+        # for each cell
+        for y in range(9):
+            for x in range(9):
 
-    shadow = blank
+                # already solved
+                if self.board[y][x] != 0: continue
+                
+                # guessed wrong
+                if not self.possibilities[y][x]: raise Exception("Wrong guess")
 
-print("\nHere you go:")
-for y in range(9):
-    for x in range(9):
-        if not x:
-            print()
-        print(board[y][x], end=' ')
+                # save current state minus the guess
+                checkpointBoard = [row[:] for row in self.board]
+                checkpointPossibilities = [[cell[:] for cell in row] for row in self.possibilities]
+                checkpointPossibilities[y][x] = self.possibilities[y][x][1:]
+                self.checkpoints.append((checkpointBoard, checkpointPossibilities))
+
+                # make a guess
+                self.board[y][x] = self.possibilities[y][x][0]
+                self.possibilities[y][x] = []
+                return
+    
+    def solve(self) -> None:
+        """Solve the board with multiple different algorithms (including blind trial and error)."""
+        skipToGuess = False # when a guess is wrong, try again
+
+        while True:
+            try:
+                if not skipToGuess:
+                    if self.solveCells(): continue
+                    if self.solveMissingNumbers(): continue
+                    if self.isSolved(): return
+
+                else:
+                    skipToGuess = False
+
+                self.guess()
+
+            except Exception:
+                try:
+                    self.board, self.possibilities = self.checkpoints.pop()
+                
+                except IndexError:
+                    print("No solution found for this sudoku game.")
+                    return
+                
+                skipToGuess = True
+
+
+
+if __name__ == "__main__": Game()
